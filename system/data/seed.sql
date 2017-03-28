0e5de0e94ae558bfdb28cab51faeaef93fda796a
Jan Polak <jan.polak@onmyway.cz>
2017-03-28 14:43:36 +0200

    players table seed

---
+++ system/data/seed.sql
@@ -0,0 +1,7 @@
+# players
+
+INSERT INTO `players` (`id`, `name`, `user`, `password`) VALUES
+(1, 'John', NULL, NULL),
+(2, 'Bob', NULL, NULL),
+(3, 'Kevin', NULL, NULL),
+(4, 'Stuart', NULL, NULL);
